b248aef17281c67dc2a9eeb2e060ebe3cc95fd13
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-05-05 13:29:07 +0300

    wip towards 12.14: extract Todo component

--- todo-app/todo-frontend/src/setupTests.js
+++ todo-app/todo-frontend/src/setupTests.js
@@ -2,4 +2,14 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+import React from 'react'
+
+import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/react'
+
+import Todo from '../src/Todos/Todo'
+
+test('make some test for Todo component', () => {
+  expect(element).toBeDefined()
+})
