706444fe6755da116462f8e4db463b86fe9a1e7c
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-05-05 09:34:59 +0300

    12.10

---
+++ todo-app/todo-backend/routes/statistics.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { Todo } = require('../mongo')
+const { getAsync, setAsync } = require('../redis/index')
+const router = express.Router();
+
+// This should probably be in a single place
+// We'll optimize it later if need be
+const todoCounterKey = 'todo_counter_key'
+
+/* GET added todos counter. */
+router.get('/', async (_, res) => {
+  const todosCounter = await getAsync(todoCounterKey)
+  const todosCounterObject = {
+    added_todos: todosCounter
+  }
+  res.send(todosCounterObject);
+});
+
+module.exports = router;
